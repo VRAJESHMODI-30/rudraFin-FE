56b77ba8c5c4a0fdd0bfb98493a2dcfa860885f4
Vrajesh Modi <vrajeshmodi99@gmail.com>
2023-10-02 14:28:53 +0530

    changes

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,9 +1,9 @@
 // Partner.js
-import React, { useRef } from 'react';
-import '../styles/Partner.css';
-import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useRef } from "react";
+import "../styles/Partner.css";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Partner = () => {
   const nameRef = useRef();
@@ -18,26 +18,32 @@ const Partner = () => {
     try {
       const formData = new FormData();
 
-      formData.append('name', nameRef.current.value);
-      formData.append('email', emailRef.current.value);
-      formData.append('phone', phoneRef.current.value);
-      formData.append('address', addressRef.current.value);
-      formData.append('message', messageRef.current.value);
+      formData.append("name", nameRef.current.value);
+      formData.append("email", emailRef.current.value);
+      formData.append("phone", phoneRef.current.value);
+      formData.append("address", addressRef.current.value);
+      formData.append("message", messageRef.current.value);
 
-      const response = await axios.post(process.env.REACT_APP_API_URL+route, formData, {
-        headers: {
-          'Content-Type': 'application/json', // Set the appropriate Content-Type
-        },
-      });
+      const response = await axios.post(
+        process.env.REACT_APP_API_URL + route,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json", // Set the appropriate Content-Type
+          },
+        }
+      );
 
       if (response.status === 200) {
-        nameRef.current.value = '';
-        emailRef.current.value = '';
-        phoneRef.current.value = '';
-        addressRef.current.value = '';
-        messageRef.current.value = '';
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+        phoneRef.current.value = "";
+        addressRef.current.value = "";
+        messageRef.current.value = "";
 
-        toast.success("Your appointment request has been sent successfully. Thank you!");
+        toast.success(
+          "Your appointment request has been sent successfully. Thank you!"
+        );
       }
     } catch (error) {
       console.error(error);
@@ -55,21 +61,48 @@ const Partner = () => {
         <div className="container">
           <div className="section-title">
             <h2>Become a Partner</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem...</p>
+            <p>
+              Drop your details below to set up an appointment with us. We'll
+              get back to you as soon as possible!
+            </p>
           </div>
 
-          <form onSubmit={(e) => sendRequest('/become-partner', e)} method="post" role="form" className="php-email-form">
+          <form
+            onSubmit={(e) => sendRequest("/become-partner", e)}
+            method="post"
+            role="form"
+            className="php-email-form"
+          >
             <div className="row">
               <div className="col-md-4 form-group">
-                <input ref={nameRef} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <input
+                  ref={nameRef}
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  id="name"
+                  placeholder="Your Name"
+                  data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars"
+                />
                 <div className="validate"></div>
               </div>
               <div className="col-md-4 form-group mt-3 mt-md-0">
-                <input ref={emailRef} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                <input
+                  ref={emailRef}
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                  data-rule="email"
+                  data-msg="Please enter a valid email"
+                />
                 <div className="validate"></div>
               </div>
               <div className="col-md-4 form-group mt-3 mt-md-0">
-                <input ref={phoneRef}
+                <input
+                  ref={phoneRef}
                   type="tel"
                   className="form-control"
                   name="phone"
@@ -78,26 +111,46 @@ const Partner = () => {
                   data-rule="minlen:4"
                   data-msg="Please enter at least 4 chars"
                   inputMode="numeric"
-                  pattern="[0-9]{10}" />
+                  pattern="[0-9]{10}"
+                />
                 <div className="validate"></div>
               </div>
             </div>
             <div className="row">
               <div className="col-md-8 form-group mt-3">
-                <input ref={addressRef} type="address" name="date" className="form-control datepicker" id="date" placeholder="Address" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <input
+                  ref={addressRef}
+                  type="address"
+                  name="date"
+                  className="form-control datepicker"
+                  id="date"
+                  placeholder="Address"
+                  data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars"
+                />
                 <div className="validate"></div>
               </div>
             </div>
             <div className="form-group mt-3">
-              <textarea ref={messageRef} className="form-control" name="message" rows="5" placeholder="Message (Optional)"></textarea>
+              <textarea
+                ref={messageRef}
+                className="form-control"
+                name="message"
+                rows="5"
+                placeholder="Message (Optional)"
+              ></textarea>
               <div className="validate"></div>
             </div>
             <div className="mb-3">
               <div className="loading">Loading</div>
               <div className="error-message"></div>
-              <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
+              <div className="sent-message">
+                Your appointment request has been sent successfully. Thank you!
+              </div>
             </div>
-            <div className="text-center"><button type="submit">Submit</button></div>
+            <div className="text-center">
+              <button type="submit">Submit</button>
+            </div>
           </form>
         </div>
       </section>
